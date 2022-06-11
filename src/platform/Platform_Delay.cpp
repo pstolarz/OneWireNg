e9bb055fe3b07373e217f2343e864c2e5cc0bbd8
Piotr Stolarz <pstolarz@o2.pl>
2022-06-12 01:13:15 +0200

    ESP32/ESP8266: CPU freq retrieved on runtime

--- src/platform/Platform_Delay.cpp
+++ src/platform/Platform_Delay.cpp
@@ -12,6 +12,59 @@
 
 #include "platform/Platform_Delay.h"
 
+#if CONFIG_BITBANG_DELAY_CCOUNT
+# if defined(ARDUINO_ARCH_ESP8266) || defined(CONFIG_IDF_TARGET_ESP8266)
+unsigned cpuFreqMhz = 0;
+unsigned ccntAdjst = 0;
+
+#  ifdef ARDUINO
+#   include "core_esp8266_features.h"
+
+unsigned ccntUpdateCpuFreqMHz(void)
+{
+    cpuFreqMhz =  (unsigned)esp_get_cpu_freq_mhz();
+    ccntAdjst = 15;
+    return cpuFreqMhz;
+}
+#  else
+/* IDF: ESP8266 RTOS SDK */
+#   include "driver/rtc.h"
+
+unsigned ccntUpdateCpuFreqMHz(void)
+{
+    switch (rtc_clk_cpu_freq_get())
+    {
+    case RTC_CPU_FREQ_80M:
+        cpuFreqMhz =  80;
+        break;
+
+    case RTC_CPU_FREQ_160M:
+        cpuFreqMhz =  160;
+        break;
+    }
+    ccntAdjst = 15;
+    return cpuFreqMhz;
+}
+#  endif
+# elif defined(ARDUINO_ARCH_ESP32) || defined(IDF_VER)
+#  include "soc/rtc.h"
+
+unsigned cpuFreqMhz = 0;
+unsigned ccntAdjst = 0;
+
+unsigned ccntUpdateCpuFreqMHz(void)
+{
+    rtc_cpu_freq_config_t conf;
+    rtc_clk_cpu_freq_get_config(&conf);
+
+    cpuFreqMhz =  (unsigned)conf.freq_mhz;
+    ccntAdjst = (cpuFreqMhz >= 20 ? 15 : 0);
+
+    return cpuFreqMhz;
+}
+# endif
+#endif /* CONFIG_BITBANG_DELAY_CCOUNT */
+
 #ifdef IDF_VER
 # include "esp_timer.h"
 
